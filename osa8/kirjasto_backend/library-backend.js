8dfe87b9780ceee86b1d0e938623e2f82da7905c
Nico Kääriäinen <63149424+nicokaariainen@users.noreply.github.com>
2023-05-16 14:12:55 +0300

    osa8 db setup

--- osa8/kirjasto_backend/library-backend.js
+++ osa8/kirjasto_backend/library-backend.js
@@ -80,6 +80,24 @@ let books = [
   },
 ]
 
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", false)
+
+const Book = require("./models/book")
+const Author = require("./models/author")
+
+const { MONGODB_URI } = process.env.MONGODB_URI
+console.log("connecting to", MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB")
+  })
+  .catch((error) => {
+    console.log("error connection to MongoDB:", error.message)
+  })
+
 const typeDefs = `
   type Query {
     bookCount: Int!
@@ -90,15 +108,17 @@ const typeDefs = `
 
   type Book {
     title: String!
-    author: String!
+    author: Author!
     published: Int!
     genres: [String!]!
+    id: ID!
   }
 
   type Author {
     name: String!
     born: Int
     bookCount: Int!
+    id: ID!
   }
 
   type Mutation {
