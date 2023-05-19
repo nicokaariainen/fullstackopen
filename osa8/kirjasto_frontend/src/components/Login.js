39da1a64b08925c625fd13797c619b7a3bf6019d
Nico Kääriäinen <63149424+nicokaariainen@users.noreply.github.com>
2023-05-19 15:14:49 +0300

    osa8 8.18

---
+++ osa8/kirjasto_frontend/src/components/Login.js
@@ -0,0 +1,64 @@
+import { useMutation } from "@apollo/client"
+import { LOGIN } from "../queries"
+import { useState, useEffect } from "react"
+
+const Login = ({ show, setToken, setPage }) => {
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      console.log(error.graphQLErrors[0].message)
+    },
+  })
+
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value
+      setToken(token)
+      localStorage.setItem("library-user-token", token)
+    }
+  }, [result.data]) // eslint-disable-line
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  if (!show) {
+    return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    login({
+      variables: {
+        username,
+        password,
+      },
+    })
+    setPage("authors")
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={submit}>
+        <div>
+          username{" "}
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password{" "}
+          <input
+            value={password}
+            type="password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
